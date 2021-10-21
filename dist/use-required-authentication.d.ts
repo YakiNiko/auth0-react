4bd156d25b96a59505d89105ea16f20e4d6bc694
Nicolas Grégoire <nicolas.gregoire@hikipeople.com>
2021-10-21 11:08:00 +0200

    chore(*): add compiled files

---
+++ dist/use-required-authentication.d.ts
@@ -0,0 +1,59 @@
+/// <reference types="react" />
+import { RedirectLoginOptions, User } from '@auth0/auth0-spa-js';
+/**
+ * Options for the withAuthenticationRequired Higher Order Component
+ */
+export interface WithAuthenticationRequiredOptions {
+  /**
+   * ```js
+   * withAuthenticationRequired(Profile, {
+   *   returnTo: '/profile'
+   * })
+   * ```
+   *
+   * or
+   *
+   * ```js
+   * withAuthenticationRequired(Profile, {
+   *   returnTo: () => window.location.hash.substr(1)
+   * })
+   * ```
+   *
+   * Add a path for the `onRedirectCallback` handler to return the user to after login.
+   */
+  returnTo?: string | (() => string);
+  /**
+   * ```js
+   * withAuthenticationRequired(Profile, {
+   *   onRedirecting: () => <div>Redirecting you to the login...</div>
+   * })
+   * ```
+   *
+   * Render a message to show that the user is being redirected to the login.
+   */
+  onRedirecting?: () => JSX.Element;
+  /**
+   * ```js
+   * withAuthenticationRequired(Profile, {
+   *   loginOptions: {
+   *     appState: {
+   *       customProp: 'foo'
+   *     }
+   *   }
+   * })
+   * ```
+   *
+   * Pass additional login options, like extra `appState` to the login page.
+   * This will be merged with the `returnTo` option used by the `onRedirectCallback` handler.
+   */
+  loginOptions?: RedirectLoginOptions;
+  /**
+   * Check the user object for JWT claims and return a boolean indicating
+   * whether or not they are authorized to view the component.
+   */
+  claimCheck?: (claims?: User) => boolean;
+}
+export declare const useRequiredAuthentication: (
+  options: WithAuthenticationRequiredOptions
+) => boolean;
+//# sourceMappingURL=use-required-authentication.d.ts.map
